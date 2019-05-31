f2ddb65e2c729994af55ba462443b2bb80cd867c
Maxime Roy <maxime.roy@fxinnovation.com>
2019-05-31 11:12:49 -0400

    Formatted, change source of module

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 locals {
-    default_ssh_public_key  = "${file("~/.ssh/id_rsa.pub")}"
-    ssh_public_key          = "${var.ssh_public_key != "" ? var.ssh_public_key : local.default_ssh_public_key }"
+  default_ssh_public_key = "${file("~/.ssh/id_rsa.pub")}"
+  ssh_public_key         = "${var.ssh_public_key != "" ? var.ssh_public_key : local.default_ssh_public_key }"
 }
 
 module "service_principal" {
-    source = "service_principal"
-    sp_name             = "${var.cluster_name}"
+  source  = "../terraform-module-azure-service_principal"
+  sp_name = "${var.cluster_name}"
 }
 
 resource "azurerm_resource_group" "aks" {
-    name     = "${var.resource_group_name}"
-    location = "${var.location}"
-    tags     = "${var.tags}"
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+  tags     = "${var.tags}"
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
@@ -21,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = "${var.cluster_name}"
 
   linux_profile {
-    admin_username  = "${var.admin_username}"
+    admin_username = "${var.admin_username}"
 
     ssh_key {
       key_data = "${local.ssh_public_key}"
@@ -37,9 +37,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   service_principal {
-    client_id       = "${module.service_principal.client_id}"
-    client_secret   = "${module.service_principal.client_secret}"
+    client_id     = "${module.service_principal.client_id}"
+    client_secret = "${module.service_principal.client_secret}"
   }
 
-  tags              = "${var.tags}"
+  tags = "${var.tags}"
 }
--- variables.tf
+++ variables.tf
@@ -1,54 +1,55 @@
 variable "resource_group_name" {
-    type = "string"
-    default =   "aks"
-    description = "Name of the resource group that will be created and host with the cluster."
+  type        = "string"
+  default     = "aks"
+  description = "Name of the resource group that will be created and host with the cluster."
 }
 
 variable "cluster_name" {
-    type = "string"
-    default = "clustername"
-    description = "Name of the AKS cluster to deploy. This will be used also as the DNS prefix."
+  type        = "string"
+  default     = "clustername"
+  description = "Name of the AKS cluster to deploy. This will be used also as the DNS prefix."
 }
 
 variable "location" {
-    type = "string"
-    default =   "westeurope"
-    description = "Location where the resource group and cluster will be deployed."
+  type        = "string"
+  default     = "canadacentral"
+  description = "Location where the resource group and cluster will be deployed."
 }
 
 variable "admin_username" {
-    type = "string"
-    default = "aksadmin"
-    description = "Username that will be used to access the cluster."
+  type        = "string"
+  default     = "aksadmin"
+  description = "Username that will be used to access the cluster."
 }
 
 variable "ssh_public_key" {
-   type         = "string"
-   default      = ""
-   description  = "Public key for aksadmin's SSH access.  Will default to the contents of ~/.ssh/id_rsa.pub."
+  type        = "string"
+  default     = ""
+  description = "Public key for aksadmin's SSH access.  Will default to the contents of ~/.ssh/id_rsa.pub."
 }
 
 variable "agent_count" {
-    default =   2
-    description = "Amount of VMs that will be deployed to create the cluster."
+  default     = 2
+  description = "Amount of VMs that will be deployed to create the cluster."
 }
 
 variable "vm_size" {
-    type = "string"
-    default =   "Standard_DS2_v2"
-    description = "Sizing of each of the agents that makes the cluster."
+  type        = "string"
+  default     = "Standard_DS2_v2"
+  description = "Sizing of each of the agents that makes the cluster."
 }
 
 variable "vm_os_disk_gb_size" {
-    default = 30
-    description = "Size of the OS disk for each agent. "
+  default     = 30
+  description = "Size of the OS disk for each agent. "
 }
 
 variable "tags" {
-    default     = {
-        FXOwner  = "Name"
-        FXDepartment = "cloud"
-        FXProject = "FXCL"
-    }
-    description = "Tags that will be applied to the resource group and cluster. The default contains the tags that fits with FX's policies."
+  default = {
+    FXOwner      = "Name"
+    FXDepartment = "cloud"
+    FXProject    = "FXCL"
+  }
+
+  description = "Tags that will be applied to the resource group and cluster. The default contains the tags that fits with FX's policies."
 }

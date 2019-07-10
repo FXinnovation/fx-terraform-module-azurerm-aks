a73ecc0681ab9cbe497bb51d8ae2c4d964d48091
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-07-10 11:32:20 -0400

    Add inspec tests for example

---
+++ examples/default/controls/default.rb
@@ -0,0 +1,10 @@
+#
+# inspec_profile::azurerm-aks-default
+# controls::default
+#
+# author::cloudsquad
+# description:: Inspec controls for azuread-service-principal-default
+#
+
+include_controls 'azurerm-aks'
+include_controls 'azuread-service-principal'
